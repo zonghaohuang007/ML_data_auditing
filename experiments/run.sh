c05f7fd4069144757f0d35e15c53fcda7a58bede
zonghaohuang007 <huang_zonghao@hotmail.com>
2024-08-11 22:36:22 -0400

    update run.sh by adding notes on what to use to reproduce main results

--- experiments/run.sh
+++ experiments/run.sh
@@ -1,31 +1,32 @@
 # Please note that it will cost weeks or even months to finish all the experiments. It is better to run them in parallel to save time.
 
-# auditing cifar10 in image classifier
+# To reproduce results in table 2: overall performance of our proposed method applied in image classifiers on different image benchmarks
+## auditing cifar10 in image classifier
 python3 ./experiments/classifier_cifar10.py --data_path <path where cifar-10-python.tar.gz is downloaded and unzipped> --mark_budget 0.1 --radius 10 --K 16 --p 0.05 --num_experiments 20
-
-# auditing cifar100 in image classifier
+## auditing cifar100 in image classifier
 python3 ./experiments/classifier_cifar100.py --data_path <path where cifar-100-python.tar.gz is downloaded and unzipped> --mark_budget 0.1 --radius 10 --K 16 --p 0.05 --num_experiments 20
-
-# auditing tinyimagenet in image classifier
+## auditing tinyimagenet in image classifier
 python3 ./experiments/classifier_tinyimagenet.py --data_path <path where tiny-imagenet-200.zip is downloaded and unzipped> --mark_budget 0.1 --radius 10 --K 16 --p 0.05 --num_experiments 20
 
-# auditing cifar10 in visual encoder trained by simclr
+
+# To reproduce results in table 6: Results on auditing data in visual encoder trained by SimCLR
+## auditing cifar10 in visual encoder trained by simclr
 python3 ./experiments/simclr_cifar10.py --mark_budget 0.1 --radius 10 --K 64 --p 0.05 --num_experiments 20
-
-# auditing cifar100 in visual encoder trained by simclr
+## auditing cifar100 in visual encoder trained by simclr
 python3 ./experiments/simclr_cifar100.py --mark_budget 0.1 --radius 10 --K 64 --p 0.05 --num_experiments 20
-
-# auditing tinyimagenet in visual encoder trained by simclr
+## auditing tinyimagenet in visual encoder trained by simclr
 python3 ./experiments/simclr_tinyimagenet.py --mark_budget 0.1 --radius 10 --K 64 --p 0.05 --num_experiments 20
 
-# auditing sst2 in llama 2
+
+# To reproduce results in table 7: Overall performance of our proposed method on Llama 2 fine-tuned on marked text datasets
+## auditing sst2 in llama 2
 python3 ./experiments/llama2_sst2.py --mark_budget 0.1 --p 0.05 --num_experiments 20
-
-# auditing agnews in llama 2
+## auditing agnews in llama 2
 python3 ./experiments/llama2_agnews.py --mark_budget 0.1 --p 0.05 --num_experiments 20
-
-# auditing tweet in llama 2
+## auditing tweet in llama 2
 python3 ./experiments/llama2_tweet.py --mark_budget 0.1 --p 0.05 --num_experiments 20
 
-# auditing flickr30k in clip
+
+# To reproduce results in table 8:  Overall performance of our proposed method on CLIP fine-tuned on marked Flickr30k
+## auditing flickr30k in clip
 python3 ./experiments/clip_flickr.py --mark_budget 0.1 --radius 10 --p 0.05 --num_experiments 20
